958e4f69c86e5732ea43b9157203af9f67c577e5
keeshore <keeshore21edu@gmail.com>
2025-11-08 23:31:27 +0530

    checking if input is digit or alpha

--- Shopping Cart Manager using List & Dictionary.py
+++ Shopping Cart Manager using List & Dictionary.py
@@ -11,35 +11,41 @@ while True:
         print("\n{}.{}".format(count, lst))
 
     print("\nEnter you choice for next move")
-    choice = int(input("press the option (1,2,3,4) :"))
+    choice = input("press the option (1,2,3,4) :")
 
-    if choice >= 5 and choice >=0:
-       print("choose the correct option")
+    if not choice.isdigit():
+       print("choose the correct option (1,2,3,4)")
+       time.sleep(2)
+       continue
+
+    choice = int(choice)
+
+    if choice > 1 and choice > 4:
+       continue
+
+          
+    if choice == 1:
+       added_item = input("\nenter the items:")
+
+       if added_item in card:
+         card[added_item] += 1
+       else:
+         card[added_item] = 1
+       print(f"successfully added {added_item}")
        time.sleep(2)
 
-    else:
-              
-        if choice == 1:
-           added_item = input("\nenter the items:")
-    
-           if added_item in card:
-             card[added_item] += 1
-           else:
-             card[added_item] = 1
-            #print("successfully added")
-    
-        if choice == 2:
-           remove_item = input("\nenter the items to remove form you card:")
-           card[remove_item] -= 1
-           if card[remove_item] == 0:
-            card.pop(remove_item) 
-            print("successfully remove")
-    
-        if choice == 3:
-           print(card)
-           time.sleep(2)
-    
-        if choice == 4:
-            print("Thank you for using Shopping Cart Manager!")
-            break
-    
+    if choice == 2:
+       remove_item = input("\nenter the items to remove form you card:")
+       card[remove_item] -= 1
+       if card[remove_item] == 0:
+        card.pop(remove_item) 
+        print("successfully remove")
+        time.sleep(2)
+
+    if choice == 3:
+       print(card)
+       time.sleep(2)
+
+    if choice == 4:
+        print("Thank you for using Shopping Cart Manager!")
+        break
