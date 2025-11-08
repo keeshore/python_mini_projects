98d41d9ca8852f8e40dc95897ea38664afdaae63
keeshore <keeshore21edu@gmail.com>
2025-11-08 21:16:08 +0530

    Updated grade calculation logic

--- Student Result Evaluator.py
+++ Student Result Evaluator.py
@@ -5,7 +5,7 @@ start =int(input("press 1 to go next:"))
 
 while start == 1:
     name = input("Enter the student name: ")
-    time.sleep(2)
+    time.sleep(1)
     english_mark = float(input("Enter the English subject Mark:"))
     
     tamil_mark = float(input("enter the tamil subject mark: "))
@@ -18,7 +18,24 @@ while start == 1:
     
     physics_mark = float(input("enter the physics subject mark:"))
 
-    print(f"The total Mark of {name}:{english_mark + tamil_mark + maths_mark + biology_mark + social_mark + physics_mark}/600")
+    total_mark = ((english_mark + tamil_mark + maths_mark + biology_mark + social_mark + physics_mark)/600)*100
+    print(f"The total Mark of {name}:{total_mark} out of 600")
+
+    if total_mark >= 0 and total_mark <= 34:
+        print(f"Grad of the {name}: f")
+
+    elif total_mark >=35 and total_mark <= 59:
+        print(f"Grad of the {name}: d")
+
+    elif total_mark >=  60 and total_mark <= 70:
+        print(f"Grad of the {name}: C")
+
+    elif total_mark >= 71 and total_mark <= 89:
+        print(f"Grad of the {name}: b")
+
+    elif total_mark >= 90 and total_mark <= 100:
+        print(f"Grad of the {name}: A")
+    
 
     start = int(input("press 1 calculate next one or press 2 to exit:"))
 
